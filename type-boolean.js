212de127015d0fc0319aaf5f1c92e4ef57d17f6c
Alex Novaes <alex.novaes@escola.pr.gov.br>
2024-02-22 08:55:33 -0300

    enviado 22/02

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 1;
+
+//console.log(primeiroNumero === segundoNumero)
+
+const texto1 = "Alura";
+const texto2 = "a";
+
+console.log(texto1 === texto2)
+
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 ="Olá Mundo";
+const texto2 ="Olá Mundo";
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+const citacao ='Meu nome é ';
+const meuNome = ' Alex';
+
+console.log(citacao + meuNome)
+
+
+// concatenacao - juntar duas ou mais variavies (textos, operadores, variaveis)
+//template string ou template literal
+
+
+
